1bcb9f6e228ab0fac1bb1726f8f331b3705934d9
Matt Conway <matt@conwaysplace.com>
2021-11-24 10:49:53 -0500

    fix newline in secrets, allow remote setup script

--- install/setup.sh
+++ install/setup.sh
@@ -2,24 +2,67 @@
 
 # fail fast
 set -e
+typeset -i remote=0
 
+if [[ "$0" == "sh" ]]; then
+  remote=1
+  baseurl="https://raw.githubusercontent.com/cloudtruth/argocd-cloudtruth-plugin/main/install"
+  basedir="."
+else
+  basedir=$(cd $(dirname $0) && pwd)
+fi
 
-printf "Input Cloudtruth Api Key: "
+function echoFile {
+  if ((remote)); then
+    curl -s ${baseurl}/${1}
+  else
+    cat ${basedir}/${1}
+  fi
+}
+export -f echoFile
+
+ARGO_NAMESPACE=${ARGO_NAMESPACE:-argocd}
+echo "Installing to namespace '${ARGO_NAMESPACE}', override with: ARGO_NAMESPACE=xyz setup.sh"
+echo
+echo "The requested environment variables control the plugin"
+echo "ApiKey is required, and they can all be overriden for each argocd project"
+echo
+
+printf "Input Cloudtruth Api Key (required): "
 read CLOUDTRUTH_API_KEY
-printf "Input Cloudtruth Environment (Default and can be overridden for each Argo project): "
+if [[ -z ${CLOUDTRUTH_API_KEY} ]]; then echo "Api Key is required"; exit 1; fi
+
+printf "Input Cloudtruth Environment [default]: "
 read CLOUDTRUTH_ENVIRONMENT
-printf "Input Cloudtruth Project (Default and can be overridden for each Argo project): "
+if [[ -z ${CLOUDTRUTH_ENVIRONMENT} ]]; then echo "Using 'default' for environment"; CLOUDTRUTH_ENVIRONMENT=default; fi
+
+printf "Input Cloudtruth Project [MyFirstProject]: "
 read CLOUDTRUTH_PROJECT
+if [[ -z ${CLOUDTRUTH_PROJECT} ]]; then echo "Using 'MyFirstProject' for project"; CLOUDTRUTH_PROJECT=MyFirstProject; fi
 
-cat <<EOF | kubectl apply -n argocd -f -
-$(cat argocd-cloudtruth-plugin-secret.yaml)
-  CLOUDTRUTH_API_KEY: $(echo ${CLOUDTRUTH_API_KEY} | base64)
-  CLOUDTRUTH_ENVIRONMENT: $(echo ${CLOUDTRUTH_ENVIRONMENT} | base64)
-  CLOUDTRUTH_PROJECT: $(echo ${CLOUDTRUTH_PROJECT} | base64)
+printf "Input Cloudtruth Tag []: "
+read CLOUDTRUTH_TAG
+
+secret_yaml=$(cat <<EOF
+$(echoFile argocd-cloudtruth-plugin-secret.yaml)
+  CLOUDTRUTH_API_KEY: $(echo -n ${CLOUDTRUTH_API_KEY} | base64)
+  CLOUDTRUTH_ENVIRONMENT: $(echo -n ${CLOUDTRUTH_ENVIRONMENT} | base64)
+  CLOUDTRUTH_PROJECT: $(echo -n ${CLOUDTRUTH_PROJECT} | base64)
 EOF
+)
 
-kubectl get -n argocd configmap/argocd-cm -o yaml > argocd-cm..original.$(date +%s).yaml
-kubectl patch -n argocd configmap/argocd-cm --patch "$(cat argocd-cm.patch.yaml)"
+if [[ -n ${CLOUDTRUTH_TAG} ]]; then
+secret_yaml=$(cat <<EOF
+${secret_yaml}
+  CLOUDTRUTH_TAG: $(echo -n "${CLOUDTRUTH_TAG}" | base64)
+EOF
+)
+fi
 
-kubectl get -n argocd deployment/argocd-repo-server -o yaml > argocd-repo-server.original.$(date +%s).yaml
-kubectl patch -n argocd deployment/argocd-repo-server --patch "$(cat argocd-repo-server.patch.yaml)"
+echo "${secret_yaml}" | kubectl apply -n ${ARGO_NAMESPACE} -f -
+
+kubectl get -n ${ARGO_NAMESPACE} configmap/argocd-cm -o yaml > ${basedir}/argocd-cm..original.$(date +%s).yaml
+kubectl patch -n ${ARGO_NAMESPACE} configmap/argocd-cm --patch "$(echoFile argocd-cm.patch.yaml)"
+
+kubectl get -n ${ARGO_NAMESPACE} deployment/argocd-repo-server -o yaml > ${basedir}/argocd-repo-server.original.$(date +%s).yaml
+kubectl patch -n ${ARGO_NAMESPACE} deployment/argocd-repo-server --patch "$(echoFile argocd-repo-server.patch.yaml)"
